a925d1d40faa472a4aa9f618541d8df81ff768f2
azemcov <de_la_table@mail.ru>
2026-03-31 16:24:41 +0300

    feat: schema.prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,5 @@
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
+export const prisma = new PrismaClient({ adapter });
