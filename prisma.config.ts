0caea3bf76b5807aa4b3ac21db4fb94e56cb6fe4
azemcov <de_la_table@mail.ru>
2026-04-03 16:17:42 +0300

    fix: prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DIRECT_URL'),
+    url: env('DATABASE_URL'),
   },
 });
+
+// DIRECT_URL
+// DATABASE_URL
